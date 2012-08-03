bf54d6d1c854d9f53c6fff57c6f5bc5095ea77b0
pilpul <pilpul@sippurim.de>
2012-08-03 23:53:07 +0200

    Refined Requests to MongoDB

--- assets/www/js/appkpok.js
+++ assets/www/js/appkpok.js
@@ -12,12 +12,12 @@
  * username and password to access the database can be set here and should be stored in localStorage
  */
 var ConfigSetting = Backbone.Model.extend({
-	localStorage: new Backbone.LocalStorage("kpoksettings"),
+	localStorage: new Backbone.LocalStorage("settings"),
 	defaults: {
-		baseURL: "http://localhost:27080/mailkpok/",
-		username: "USERNAME",
-		password: "PASSWORD"
-	}
+		baseURL: "http://URL-SLEEPY.MONGOOSE:27080/DATABASE/",
+		username: "USERNAME_WITH_READ_ACCESS",
+		password: "USERS_PASSWORD"
+	},
 	update :function(){
 		this.set({baseURL: $('#baseURL').val(), username: $('#username').val(), password: $('#password').val() });
 		this.save();
@@ -27,7 +27,6 @@ var ConfigSetting = Backbone.Model.extend({
 
 var Config = new ConfigSetting({id:	1});
 Config.fetch();
-console.log(Config.get("baseURL"))
 
 $( '#ConfigDialog' ).live( 'pageinit',function(event){
   $('#baseURL').val(Config.get("baseURL"))
@@ -36,6 +35,7 @@ $( '#ConfigDialog' ).live( 'pageinit',function(event){
 });
 
 
+	
 /**
  * Backbone-Model: Mail
  * Description: Model for an e-mail.  
@@ -59,15 +59,15 @@ var Mail = Backbone.Model.extend({
 	    return true;
 	  },
     dateCategory: function(){
-    	today = new Date();
+    	daysPerMonth = new Array(31,28,31,30,31,30,31,31,30,31,30,31)
+    	now = new Date();
     	date = new Date(this.get('date'))
-    	if (date.toDateString() == today.toDateString()){
+    	if (date.toDateString() == now.toDateString()){
     		return "archive_today"
-    	}else if(date.getDate() == today.getDate()-1 && date.getMonth() == today.getMonth()){
-    		//TODO: Yesterday could also have been the last day of another month
+    	}else if( (   date.getDate() == now.getDate()-1 && date.getMonth() == now.getMonth()  ) 
+    			    || date.getDate() == daysPerMonth[now.getMonth()-1] && now.getDate()==0){
     		return "archive_yesterday"
-    	}else if(date.getTime() > today.getTime()-(7*24*60*60*1000)	){
-    		//TODO: last != "in the last seven days")
+    	}else if(date.getTime() > now.getTime()-(7*24*60*60*1000)	){
     		return "archive_lastweek"
     	}else{
     		return "archive_old"
@@ -87,57 +87,81 @@ var MailDBStore = Backbone.Collection.extend({
 	//Sorts the collection automatically by date
 	comparator : function(mail) {
 	  milli = new Date(mail.get("date"));
-	  return milli.getTime();
+	  return -milli.getTime();
 	},
-
-	update: function(){
-		console.log('Requesting data ..')
-		var baseURL = "";
-		//Authentication TODO
+	authenticate: function(){
+    	//Authentication TODO
 		//"Cross Domain" Post messages only works in PhoneGap not in native browsers..
-		$.post('Config.get('baseURL')+'/_authenticate', {"username":Config.get('username'), "password":Config.get('password')},function(data){}, 'json')
+		$.post(Config.get('baseURL')+'_authenticate', {"username":Config.get('username'), "password":Config.get('password')},function(data){
+			console.log("authenticating")
+		}, 'jsonp')
 		.success(function(data){
-			console.log(data)
+			console.log(data.ok)
 		})
-		//TODO This answers with a list of ALL keys. Better: Only send the changes
-	    $.post(Config.get('baseURL')+'/mails/_find',function(data){
-	    	//loading message
-	    	console.log('Retrieving Maildatabase..')
-	    	$.mobile.showPageLoadingMsg()
-	    	//$('#archive_content').empty()
-	    }, 'jsonp')
+		.error(function(){
+			console.error('Error Authenticating')
+		})
+	},
+	update: function(){
+		//TODO backbone convention is to do this in 'fetch' an define URL parameters for the collection
+		$.mobile.showPageLoadingMsg('b','Loading',false)
+	    $.post(Config.get('baseURL')+'mails/_find?sort={"_id"%3A-1}', {'limit': 10}, function(data){}, 'jsonp')
+	    	.success(function(data){
+	    		//TODO make this work in something like this.bulkAdd() to be reusable
+		    	if(data.ok != 1){
+		    		console.error(data.errmsg);
+		    	}else{
+		    		console.log('processing answer..')
+			    	newmails = 0;
+			    	$.each(data.results, function(i, mail){
+			    		if(MailDB.where({date: mail.date}).length<1){
+			    			newmails++;
+			        			newmail = new Mail(mail);
+			    				MailDB.add(newmail)
+			    				newmail.save();	
+			    		}
+			    	})
+			    	console.log(newmails + ' new Items')
+			    	App.render(); //re-render the app after updates from Database
+		    	}
+		    	$.mobile.hidePageLoadingMsg()	
+	    	})
+		    .error( function(){
+		    	console.error('Error connecting to database. See answer to request for details')
+		    	console.log('Trying (re-)authentication')
+		    	this.authenticate();
+		    }
+	    )	
+	},
+	getOlder: function(){
+		lastEntryNr = MailDB.length/10;
+	    $.post(Config.get('baseURL')+'mails/_find?sort={"_id"%3A-1}', {'limit': 10, 'skip':MailDB.length}, function(data){}, 'jsonp')
 	    .success(function(data){
 	    	if(data.ok != 1){
 	    		console.error(data.errmsg);
 	    	}else{
-	    		
+	    		console.log('processing answer..')
 		    	newmails = 0;
 		    	$.each(data.results, function(i, mail){
-		    		//TODO: Request the last 10 Mails, make 'next page'; Mongo allows this with the _limit parameter
-		    		//Only request those mails that are not already in the localStorage
 		    		if(MailDB.where({date: mail.date}).length<1){
 		    			newmails++;
-		    			//console.log("Requesting id: "+mail.id)
-		    			//$.get('http://localhost:5984/couchbdkit_test/'+mail.id, function(data){;
-			    		//}, 'jsonp')
-			    		//.success(function(data){
 		        			newmail = new Mail(mail);
 		    				MailDB.add(newmail)
-		    				newmail.save();
-		    				
-			    			
-			    		//})	
+		    				newmail.save();	
 		    		}
-		    		
 		    	})
 		    	console.log(newmails + ' new Items')
-		    	App.render(); //re-render the app after updates from Databse
+		    	App.render(); //re-render the app after updates from Database
 	    	}
-	    	$.mobile.hidePageLoadingMsg()		    		
-	    })	
-	    .error(function(){
+	    	$.mobile.hidePageLoadingMsg()	
+	    })
+    	.error( function(){
 	    	console.error('Error connecting to database. See answer to request for details')
-	    })		
+	    	//this.authenticate();
+	    })
+	},
+	bulkAdd: function(data){
+		//This is somehow not referenceable from within the post.success' above
 	}
 });
 var MailDB = new MailDBStore;
@@ -162,26 +186,33 @@ var MailView = Backbone.View.extend({
  * 
  */
 var AppView = Backbone.View.extend({
+	tplArchiveDivider: _.template($('#tpl_archiveDividers').html()),
 	
 	el: $("#MailList"),
+	
 	initialize: function() {
 		MailDB.fetch(); //get it from local store TODO: should be applied at start with AppView Init
+		$('#archive_content').html(this.tplArchiveDivider());
 		console.log('Creating view');
 		this.addAll()
 	},
-	addOne: function(mail) {		  
+	addOne: function(mail) {
 	      var view = new MailView({model: mail});
-	      $("#archive_content").append(view.render().el);
+	      $("#"+mail.dateCategory()).after(view.render().el);
+	      //TODO when updating without reload the "after" pastes older mails BEFORE the newer ones..
+	      $("#"+mail.dateCategory()).css('display','block')
 	    },
 	addAll: function() {
-		  console.log("Adding all")
+		  console.log("Adding everything from localStorage to View")
 	      MailDB.each( this.addOne )
 	    },
 	render: function(){
 		console.log("Re-rendering AppView")
 		$('#archive_content').empty();
+		$('#archive_content').html(this.tplArchiveDivider());
 		this.addAll();
 		//re-rendering the collapsibles with jQM
+		//TODO the height of dividers is wrong after first refreshe
 		$('div[data-role=collapsible]').collapsible({refresh:true});
 	}
 })
